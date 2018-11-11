d44acd334fc414314f28d35fe847110513157941
Denis Zhdanov <denzhdanov@gmail.com>
2018-11-11 20:16:29 +0800

    testcontainer kafka setup

--- docker/testcontainer-kafka-sample/src/main/kotlin/org/denis/Start.kt
+++ docker/testcontainer-kafka-sample/src/main/kotlin/org/denis/Start.kt
@@ -1,17 +1,22 @@
 package org.denis
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
 
 fun main(args: Array<String>) {
+
+    val network = Network.newNetwork()
+
     val zookeeper = GenericContainer<Nothing>("bitnami/zookeeper:3.4.12").apply {
         addExposedPort(2181)
+        withNetwork(network)
         withNetworkAliases("zookeeper")
         addEnv("ALLOW_ANONYMOUS_LOGIN", "yes")
     }
     zookeeper.start()
 
     val kafka = GenericContainer<Nothing>("bitnami/kafka:2.0.0").apply {
-        withNetwork(zookeeper.network)
+        withNetwork(network)
         addExposedPort(9092)
         withNetworkAliases("kafka")
         addEnv("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")

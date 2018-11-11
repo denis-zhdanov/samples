011b0e42c3793f36758742d80b9c7e1bd963fd8f
Denis Zhdanov <denzhdanov@gmail.com>
2018-11-11 18:58:23 +0800

    testcontainer kafka example

---
+++ docker/testcontainer-kafka-sample/src/main/kotlin/org/denis/Start.kt
@@ -0,0 +1,23 @@
+package org.denis
+
+import org.testcontainers.containers.GenericContainer
+
+fun main(args: Array<String>) {
+    val zookeeper = GenericContainer<Nothing>("bitnami/zookeeper:3.4.12").apply {
+        addExposedPort(2181)
+        withNetworkAliases("zookeeper")
+        addEnv("ALLOW_ANONYMOUS_LOGIN", "yes")
+    }
+    zookeeper.start()
+
+    val kafka = GenericContainer<Nothing>("bitnami/kafka:2.0.0").apply {
+        withNetwork(zookeeper.network)
+        addExposedPort(9092)
+        withNetworkAliases("kafka")
+        addEnv("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")
+        addEnv("ALLOW_PLAINTEXT_LISTENER", "yes")
+        addEnv("KAFKA_LISTENERS", "PLAINTEXT://:9092")
+        addEnv("KAFKA_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:9092")
+    }
+    kafka.start()
+}
